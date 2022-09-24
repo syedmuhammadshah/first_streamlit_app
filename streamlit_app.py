ca5981ac347b90e6754c2ac2910d6c089ccbdd94
Syed Muhammad Shah <44096671+syedmuhammadshah@users.noreply.github.com>
2022-09-24 21:58:21 +0500

    insert value into snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,6 +40,8 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
-add_my_fruit = streamlit.text_input('What fruit would you like information about?')
-all_fruits = my_data_rows.append(add_my_fruit)
-streamlit.dataframe(my_data_rows)
+my_cur.execute("insert into fruit_load_list values('from streamlit')")
+
+#add_my_fruit = streamlit.text_input('What fruit would you like information about?')
+#all_fruits = my_data_rows.append(add_my_fruit)
+#streamlit.dataframe(my_data_rows)
