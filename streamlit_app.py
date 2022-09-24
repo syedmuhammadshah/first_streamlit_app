b285bbfe7fd43efa50ca2fa1e1cd368bcd16f047
Syed Muhammad Shah <44096671+syedmuhammadshah@users.noreply.github.com>
2022-09-24 21:52:31 +0500

    User to add fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,3 +39,7 @@ my_cur.execute("SELECT * FROM fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+add_my_fruit = streamlit.text_input('What fruit would you like information about?')
+all_fruits = my_data_rows.loc[add_my_fruit]
+streamlit.dataframe(all_fruits)
