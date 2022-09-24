cc93db7096f75a1bcddc13ceb37f676177590450
Syed Muhammad Shah <44096671+syedmuhammadshah@users.noreply.github.com>
2022-09-24 17:42:31 +0500

    Fruityvise api response

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,4 +19,11 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
 
+# New section to display fruityvise api response
+streamlit.header('Fruityvise Fruit Advice')
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response.json())
+
 
